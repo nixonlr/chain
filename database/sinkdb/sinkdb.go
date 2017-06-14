c61562b67f3fd119028766e064bfa9953bef3c64
Jackson Owens <jackson@chain.com>
2017-06-14 16:53:05 -0700

    database/sinkdb: add Close method
    Add Close method to release resources associated with a sinkdb handle
    and stop its raft service. Use this Close method to write a basic unit
    test for persistence.

--- database/sinkdb/sinkdb.go
+++ database/sinkdb/sinkdb.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"net/http"
 	"sort"
+	"sync"
 
 	"github.com/golang/protobuf/proto"
 
@@ -30,8 +31,26 @@ func Open(laddr, dir string, httpClient *http.Client) (*DB, error) {
 
 // DB provides access to an opened kv store.
 type DB struct {
+	mu     sync.Mutex
+	closed bool
+
 	state *state
 	raft  *raft.Service
+}
+
+// Close closes the database handle releasing its resources. It is
+// the caller's responsibility to ensure that there are no concurrent
+// database operations in flight. Close is idempotent.
+//
+// All other methods have undefined behavior on a closed DB.
+func (db *DB) Close() error {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+	if db.closed { // make Close idempotent
+		return nil
+	}
+	db.closed = true
+	return db.raft.Stop()
 }
 
 // Exec executes the provided operations
